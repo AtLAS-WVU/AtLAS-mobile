b00e89dd2060da352e753c406ff7b5306f11b9e7
nathantspencer <nathantspencer@gmail.com>
2017-09-20 14:08:25 -0400

    added base navigation

--- AtLAS/app/src/main/java/com/nathantspencer/atlas/MainActivity.java
+++ AtLAS/app/src/main/java/com/nathantspencer/atlas/MainActivity.java
@@ -17,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_history:
+                    mTextMessage.setText(R.string.title_history);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_map:
+                    mTextMessage.setText(R.string.title_map);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_send:
+                    mTextMessage.setText(R.string.title_send);
                     return true;
             }
             return false;
