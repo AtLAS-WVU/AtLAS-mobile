09942b5e08ec6b69e698a0b26066b54aadd3cd14
nathantspencer <nathantspencer@gmail.com>
2018-03-07 22:03:05 -0500

    Added ability to add send request descriptipn

--- AtLAS/app/src/main/java/com/nathantspencer/atlas/FriendsArrayAdapter.java
+++ AtLAS/app/src/main/java/com/nathantspencer/atlas/FriendsArrayAdapter.java
@@ -4,11 +4,13 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListAdapter;
 import android.widget.TextView;
 
@@ -24,6 +26,8 @@ public class FriendsArrayAdapter extends BaseAdapter implements ListAdapter
     private ArrayList<Boolean> mIsPending = new ArrayList<>();
     private ArrayList<String> mNames = new ArrayList<>();
     private Context mContext;
+
+    private Button mSendButton;
 
     private class ConfirmFriendRequestResponder implements RequestResponder
     {
@@ -60,6 +64,18 @@ public class FriendsArrayAdapter extends BaseAdapter implements ListAdapter
             ((MainActivity) mContext).RefreshFriends();
         }
     }
+
+    private class RequestToSendRequestResponder implements RequestResponder
+    {
+        RequestToSendRequestResponder()
+        {
+        }
+
+        public void onResponse(String response)
+        {
+        }
+    }
+
 
 
     public FriendsArrayAdapter(ArrayList<String> list, ArrayList<Boolean> isPendingList, ArrayList<String> names, Context context)
@@ -106,7 +122,7 @@ public class FriendsArrayAdapter extends BaseAdapter implements ListAdapter
         Button denyButton = view.findViewById(R.id.deny_friend);
         Button confirmButton = view.findViewById(R.id.confirm_friend);
         Button deleteButton = view.findViewById(R.id.delete_friend);
-        Button sendButton = view.findViewById(R.id.send_friend);
+        mSendButton = view.findViewById(R.id.send_friend);
 
         if (!mIsPending.get(position))
         {
@@ -116,7 +132,7 @@ public class FriendsArrayAdapter extends BaseAdapter implements ListAdapter
         else
         {
             deleteButton.setVisibility(View.GONE);
-            sendButton.setVisibility(View.GONE);
+            mSendButton.setVisibility(View.GONE);
         }
 
         denyButton.setOnClickListener(new View.OnClickListener() {
@@ -174,6 +190,38 @@ public class FriendsArrayAdapter extends BaseAdapter implements ListAdapter
                             public void onClick(DialogInterface dialog, int which) {
                             }
                         }).show();
+            }
+        });
+
+        mSendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                AlertDialog.Builder sendAlert =  new AlertDialog.Builder(mContext);
+                sendAlert.setTitle("Add a Request Description");
+                sendAlert.setMessage("Type a short message describing the package you'd like to send to " + mUsernames.get(position) + ".");
+
+
+                final EditText sendMessage = new EditText(mContext);
+                sendMessage.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_FLAG_MULTI_LINE);
+                sendAlert.setView(sendMessage);
+
+                sendAlert.setPositiveButton("Send Request", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    SharedPreferences sharedPref = mContext.getSharedPreferences("AUTH", Context.MODE_PRIVATE);
+                    final String username = sharedPref.getString("atlasUsername", "");
+                    final String atlasLoginKey = sharedPref.getString("atlasLoginKey", "");
+
+                    Map<String, String> parameterBody = new HashMap<>();
+                    parameterBody.put("username", username);
+                    parameterBody.put("friend_username", mUsernames.get(position));
+                    parameterBody.put("token", atlasLoginKey);
+                    parameterBody.put("delivery_message", "New delivery request from " + username + ".");
+                    mGeneralRequest.POSTRequest("RequestToSend.php", parameterBody, new RequestToSendRequestResponder());
+                }
+                });
+
+                sendAlert.show();
             }
         });
 
